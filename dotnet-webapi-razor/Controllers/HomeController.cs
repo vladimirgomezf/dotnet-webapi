bcf64480e647548f00dff278a54803d056192b52
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2021-11-08 15:08:23 -0500

    Add models, helpers and controllers.

---
+++ dotnet-webapi-razor/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_webapi_razor.Helpers;
+using dotnet_webapi_razor.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace dotnet_webapi_razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        PersonaAPI _api = new PersonaAPI();
+
+        public async Task<IActionResult> Index()
+        {
+            List<PersonaData> persons = new List<PersonaData>();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("api/persona");
+            if (res.IsSuccessStatusCode)
+            {
+                var results = res.Content.ReadAsStringAsync().Result;
+                persons = JsonConvert.DeserializeObject<List<PersonaData>>(results);
+            }
+            return View(persons);
+        }
+    }
+}
---
+++ dotnet-webapi-razor/Models/PersonaData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_webapi_razor.Models
+{
+    public class PersonaData
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int? Edad { get; set; }
+    }
+}
