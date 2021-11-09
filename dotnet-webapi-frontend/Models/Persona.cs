c6d3dc5c0894622822d8164ac36b1c3c6ba90b80
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2021-11-08 22:52:48 -0500

    Add model Persona y las vistas relativas al Crud de Personas.

---
+++ dotnet-webapi-frontend/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_webapi_frontend.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int? Edad { get; set; }
+    }
+}
