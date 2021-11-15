f9fcf090edcb7dc2a1905b20650af427d30aeaa9
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2021-11-15 15:18:17 -0500

    add persona service

---
+++ dotnet-webapi-frontend-angular/src/app/services/persona.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonaService {
+
+  public url: string;
+
+  constructor(public httpClient: HttpClient) {
+    this.url = 'https://localhost:5001/api/Persona';
+  }
+
+  public getAll(): Observable<any> {
+    return this.httpClient.get(this.url);
+  }
+}
