48fd173a7d5e8810e78fd70352058882e2d46a49
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2021-11-15 15:19:16 -0500

    add people-list component

---
+++ dotnet-webapi-frontend-angular/src/app/components/people-list/people-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Persona } from 'src/app/domain/persona';
+import { PersonaService } from 'src/app/services/persona.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-people-list',
+  templateUrl: './people-list.component.html',
+  styleUrls: ['./people-list.component.css']
+})
+export class PeopleListComponent implements OnInit, OnDestroy {
+
+  public persona: Persona[] = [];
+  public subPersona: Subscription = new Subscription;
+
+  constructor(public personaService: PersonaService) { }
+  
+  ngOnDestroy(): void {
+    this.subPersona.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(){
+    this.subPersona = this.personaService.getAll().subscribe(data => {
+      this.persona = data;
+    });
+  }
+
+}
