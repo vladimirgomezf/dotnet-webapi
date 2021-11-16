c45071c8f251d1dd880252e3d3ce46af24fbb9e4
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2021-11-16 11:00:28 -0500

    feat: create the controller and the html code for save form

--- dotnet-webapi-frontend-angular/src/app/app.module.ts
+++ dotnet-webapi-frontend-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PeopleListComponent } from './components/people-list/people-list.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { PersonaService } from './services/persona.service';
+import { PeopleListComponent } from './components/people-list/people-list.component';
 import { PeopleSaveComponent } from './components/people-save/people-save.component';
 
 @NgModule({
@@ -18,7 +20,8 @@ import { PeopleSaveComponent } from './components/people-save/people-save.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     PersonaService
--- dotnet-webapi-frontend-angular/src/app/components/people-save/people-save.component.ts
+++ dotnet-webapi-frontend-angular/src/app/components/people-save/people-save.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Persona } from 'src/app/domain/persona';
+import { PersonaService } from 'src/app/services/persona.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-people-save',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleSaveComponent implements OnInit {
 
-  constructor() { }
+  public persona: Persona;
+
+  public showMsg: Boolean = false;
+  public msg: string = '';
+  public type: string = '';
+
+  constructor(public personaService: PersonaService, private router: Router) {
+    this.persona = new Persona(0,'',0);
+   }
 
   ngOnInit(): void {
+    this.persona = new Persona(0,'',0);
+  }
+
+  save(){
+    console.log(this.persona);
+
+    this.personaService.save(this.persona).subscribe({
+      next: () => this.router.navigate(['/people-list']),
+      error: (e) => {
+        console.log(e);
+        this.showMsg = true;
+        this.msg = 'An error has occurred in the procedure...'
+        this.type = 'danger';
+      },
+    });
   }
 
 }
