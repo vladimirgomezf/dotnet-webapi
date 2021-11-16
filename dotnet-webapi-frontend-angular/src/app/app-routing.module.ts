5fae1876d89f5bf6c7146627077a193b25af6822
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2021-11-16 10:58:54 -0500

    add the route to people-save module

--- dotnet-webapi-frontend-angular/src/app/app-routing.module.ts
+++ dotnet-webapi-frontend-angular/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PeopleListComponent } from './components/people-list/people-list.component';
+import { PeopleSaveComponent } from './components/people-save/people-save.component';
 
 const routes: Routes = [
-  {path: "people-list", component: PeopleListComponent}
+  {path: "people-list", component: PeopleListComponent},
+  {path: "people-save", component: PeopleSaveComponent}
 ];
 
 @NgModule({
